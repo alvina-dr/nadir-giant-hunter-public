2f5d6d073a4661123834bacf15de39dfa367145c
Alvina <alvinadr24@gmail.com>
2024-05-17 11:46:54 +0200

    fix: failed merge

--- Assets/Scripts/Enemies/TargetableSpot.cs
+++ Assets/Scripts/Enemies/TargetableSpot.cs
@@ -2,6 +2,7 @@ using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class TargetableSpot : MonoBehaviour
 {
@@ -13,6 +14,7 @@ public class TargetableSpot : MonoBehaviour
         Bumper = 2
     }
     public SpotType SpotCurrentType;
+    public VisualEffect VisualFX;
     [ShowIf("SpotCurrentType", SpotType.WeakSpot)]
     public EnemyWeakSpotManagement Enemy;
 
@@ -30,15 +32,20 @@ public class TargetableSpot : MonoBehaviour
                     vfx.transform.position = transform.position;
                     Enemy.Damage(this);
                 }
+                VisualFX.SendEvent("collision");
+                StartCoroutine(DestroyAfterDelay());
                 GPCtrl.Instance.TargetableSpotList.Remove(this);
-                Destroy(gameObject);
                 break;
             case SpotType.DashSpot:
                 GPCtrl.Instance.DashPause = true;
                 Time.timeScale = GPCtrl.Instance.Player.Data.slowDownTime;
+                //GPCtrl.Instance.reliefFX.customPasses.
+                GPCtrl.Instance.reliefFX.enabled = true;
                 StartCoroutine(DashSlowDown());
                 StartCoroutine(ReloadDashSpot());
-                Mesh.enabled = false;
+                GPCtrl.Instance.Player.PlayerDash.CurrentDashSpot = this;
+                VisualFX.SendEvent("collision");
+                //Mesh.enabled = false;
                 GPCtrl.Instance.TargetableSpotList.Remove(this);
                 break;
         }
@@ -55,6 +62,13 @@ public class TargetableSpot : MonoBehaviour
     {
         yield return new WaitForSecondsRealtime(GPCtrl.Instance.GeneralData.dashSpotReloadTime);
         GPCtrl.Instance.TargetableSpotList.Add(this);
-        Mesh.enabled = true;
+        //Mesh.enabled = true;
+        VisualFX.SendEvent("OnPlay");
+    }
+
+    private IEnumerator DestroyAfterDelay()
+    {
+        yield return new WaitForSecondsRealtime(1.0f);
+        Destroy(gameObject);
     }
 }
