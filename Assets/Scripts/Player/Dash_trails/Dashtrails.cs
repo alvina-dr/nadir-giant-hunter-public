6dd0df11bbd798aed7321e6bccf6abc19088b3f6
SoOoDAa <nils.puthier.auditeur@lecnam.net>
2024-05-21 14:37:15 +0200

    material changes

---
+++ Assets/Scripts/Player/Dash_trails/Dashtrails.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dashtrails : MonoBehaviour
+
+    public float activeTime = 2f;
+
+    private bool isTrailActive;
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetDownKey "space")
+    }
+}
