91abf4e19af05f17910f20a61ea0a10c990101b5
Alvina <alvinadr24@gmail.com>
2024-05-29 17:41:36 +0200

    feat: ui rebinding

--- Assets/Input/PlayerControls.cs
+++ Assets/Input/PlayerControls.cs
@@ -282,6 +282,11 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
                     ""devicePath"": ""<Keyboard>"",
                     ""isOptional"": false,
                     ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
                 }
             ]
         }
