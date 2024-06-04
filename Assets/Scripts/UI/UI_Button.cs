d67c9f792bea70af4117094effb6091becec370f
Alvina <alvinadr24@gmail.com>
2024-06-04 18:04:15 +0200

    feat: button call ui sfx hover and click

--- Assets/Scripts/UI/UI_Button.cs
+++ Assets/Scripts/UI/UI_Button.cs
@@ -6,15 +6,19 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using DG.Tweening;
 using TMPro;
+using AK.Wwise;
 
 public class UI_Button : MonoBehaviour, ISelectHandler, IDeselectHandler
 {
     [SerializeField] private TextMeshProUGUI _textMeshProUGUI;
     [SerializeField] private Image _image;
+    [SerializeField] private AK.Wwise.Event _sfxHover;
+    [SerializeField] private AK.Wwise.Event _sfxClick;
 
     private void Awake()
     {
         _image.material = new Material(_image.material);
+        GetComponent<Button>().onClick.AddListener(OnClick);
     }
 
     public void OnDeselect(BaseEventData eventData)
@@ -29,5 +33,11 @@ public class UI_Button : MonoBehaviour, ISelectHandler, IDeselectHandler
         transform.DOScale(1.1f, .3f).SetUpdate(true);
         _textMeshProUGUI.color = Color.black;
         _image.material.SetInt("_Selected", 1);
+        _sfxHover.Post(DataHolder.Instance.gameObject);
+    }
+
+    private void OnClick()
+    {
+        _sfxClick.Post(DataHolder.Instance.gameObject);
     }
 }
