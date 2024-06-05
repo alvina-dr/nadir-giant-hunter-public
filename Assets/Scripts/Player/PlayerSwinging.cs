8257c18e5a7be38f65f65faaafb2fbd48aefd409
Alvina <alvinadr24@gmail.com>
2024-06-05 11:21:50 +0200

    feat: sound integration

--- Assets/Scripts/Player/PlayerSwinging.cs
+++ Assets/Scripts/Player/PlayerSwinging.cs
@@ -208,8 +208,7 @@ public class PlayerSwinging : MonoBehaviour
         //swing direction on the y plane
         _swingOriginalDirection = Player.Mesh.forward;
         Player.PlayerMovement.CanJumpOnceInAir = true;
-        Player.SoundData.SFX_Hunter_Hook_Single_Grappled.Post(EndSwingLinePoint.gameObject);
-        Player.SoundData.SFX_Hunter_Hook_Single_Trigger.Post(gameObject);
+        Player.SoundData.SFX_Hunter_Grapple_Trigger.Post(gameObject);
         IsSwinging = true;
         EndSwingLinePoint.SetParent(hitTransform);
         EndSwingLinePoint.position = hitPoint;
@@ -242,7 +241,7 @@ public class PlayerSwinging : MonoBehaviour
     public void StopSwing(bool boost = true, bool destroyVisual = true)
     {
         if (!_springJoint) return;
-        Player.SoundData.SFX_Hunter_Hook_Single_Trigger.Post(EndSwingLinePoint.gameObject);
+        Player.SoundData.SFX_Hunter_Grapple_Release.Post(EndSwingLinePoint.gameObject);
         Player.PlayerMovement.CurrentMoveSpeed++;
         Destroy(_springJoint);
         if (destroyVisual)
