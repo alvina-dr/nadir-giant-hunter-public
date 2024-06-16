eb3366f68bb2ef16f332bf98563e5e7aa78ca4fc
JulesSarton <jjonchay12@gmail.com>
2024-06-16 17:45:16 +0200

    changes on monster

--- Assets/Scripts/Enemies/Waving.cs
+++ Assets/Scripts/Enemies/Waving.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Rendering;
 using UnityEngine.UIElements;
 using static ak.wwise.core;
 
@@ -18,6 +19,8 @@ public class TransformWaveData
     [ReadOnly]
     public Vector3 Position;
     [ReadOnly]
+    public Vector3 Transformation;
+    [ReadOnly]
     public float Delta;
     [ReadOnly]
     public float OffSet;
@@ -29,7 +32,7 @@ public class TransformWaveData
 public class WaveData
 {
     public WavingData data;
-    public List<TransformWaveData> waveTransforms = new List<TransformWaveData>();
+    public AnimationCurve waveTransformsAffect;
 }
 
 public class Waving : MonoBehaviour
@@ -50,6 +53,7 @@ public class Waving : MonoBehaviour
     {
         WavePoints();
         CleanPoints();
+        CleanTransformations();
     }
 
     private void WavePoints()
@@ -58,9 +62,9 @@ public class Waving : MonoBehaviour
         {
             Vector3 waveDirection = transform.forward * wave.data.WaveDirection.z + transform.right * wave.data.WaveDirection.x + transform.up * wave.data.WaveDirection.y;
 
-            for (int i = 0; i < wave.waveTransforms.Count; i++)
+            for (int i = 0; i < waveTransforms.Count; i++)
             {
-                TransformWaveData transformData = wave.waveTransforms[i];
+                TransformWaveData transformData = waveTransforms[i];
                 float offSet = 0;
                 float delta = 0;
                 delta = wave.data.DeltType == WavingData.DeltaType.Time ? Time.time : delta;
@@ -74,11 +78,15 @@ public class Waving : MonoBehaviour
                 
                 transformData.Delta = delta;
                 transformData.OffSet = offSet;
-                float distToFirst = Vector3.Distance(wave.waveTransforms[0].BaseLocalPosition, transformData.Transformed.position);
-                Vector3 position = transform.position + transformData.BaseLocalPosition + waveDirection * offSet * (wave.data.WaveLenght + distToFirst * 0.01f * wave.data.WaveLenghtAccumulation);
+
+                float distToFirst = Vector3.Distance(waveTransforms[0].BaseLocalPosition, transformData.Transformed.position);
+                float totalDist = Vector3.Distance(waveTransforms[0].BaseLocalPosition, waveTransforms[waveTransforms.Count-1].Transformed.position);
+                float influence = wave.waveTransformsAffect.Evaluate(distToFirst/totalDist);
+                Vector3 transformation = (waveDirection * offSet * (wave.data.WaveLenght + distToFirst * 0.01f * wave.data.WaveLenghtAccumulation)) * influence;
+                Vector3 position = transform.position + transformData.BaseLocalPosition + transformData.Transformation + transformation;
                 transformData.Position = position;
                 transformData.Transformed.position = position;
-                
+                transformData.Transformation += transformation;
             }
         }
     }
@@ -97,15 +105,27 @@ public class Waving : MonoBehaviour
             isClean = true;
             foreach (WaveData wave in waves)
             {
-                for (int i = 0; i < wave.waveTransforms.Count; i++)
+                for (int i = 0; i < waveTransforms.Count; i++)
                 {
-                    TransformWaveData transformData = wave.waveTransforms[i];
+                    TransformWaveData transformData = waveTransforms[i];
                     if (transformData.Position != transformData.Transformed.position)
                     {
                         isClean = false;
                         transformData.Transformed.position = transformData.Position;
                     }
                 }
+            }
+        }
+    }
+
+    private void CleanTransformations()
+    {
+        foreach(WaveData wave in waves)
+        {
+            for (int i = 0; i < waveTransforms.Count; i++)
+            {
+                TransformWaveData transformData = waveTransforms[i];
+                transformData.Transformation = Vector3.zero;
             }
         }
     }
@@ -125,7 +145,7 @@ public class Waving : MonoBehaviour
 
             offSet = wave.data.OndulType == WavingData.OndulationType.Cosinus ? Mathf.Cos(delta * wave.data.WaveSpeed) : offSet;
             offSet = wave.data.OndulType == WavingData.OndulationType.Sinus ? Mathf.Sin(delta * wave.data.WaveSpeed) : offSet;
-            float distToFirst = Vector3.Distance(wave.waveTransforms[0].BaseLocalPosition, leg.FirstBone.transform.position);
+            float distToFirst = Vector3.Distance(waveTransforms[0].BaseLocalPosition, leg.FirstBone.transform.position);
 
             leg.Target.position -= waveDirection * offSet * (wave.data.WaveLenght + distToFirst * 0.01f * wave.data.WaveLenghtAccumulation);
         }
@@ -135,7 +155,7 @@ public class Waving : MonoBehaviour
     {
         foreach (WaveData wave in waves)
         {
-            foreach (TransformWaveData transformData in wave.waveTransforms)
+            foreach (TransformWaveData transformData in waveTransforms)
             {
                 transformData.BaseLocalPosition = transformData.Transformed.position - transform.position;
             }
